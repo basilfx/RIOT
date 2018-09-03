b3b96fdfdc4562baf3df07c2e6d67a3211b9f2e0
Bas Stottelaar <basstottelaar@gmail.com>
2018-09-04 00:15:18 +0200

    sys: knx_device: add initial support

---
+++ sys/include/knx_device.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2018-2021 Bas Stottelaar <basstottelaar@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_knx_device    KNX Device
+ * @ingroup     sys
+ * @brief       KNX device
+ *
+ * @see         KNX Specification version 2.1, Architecture v03.00.02
+ *
+ * # Supported device models
+ * Currently the following device models are supported:
+ *
+ * * System 7: @ref sys_knx_device_system7
+ *
+ * @{
+ * @file
+ * @brief       KNX device includes
+ *
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ */
+#ifndef KNX_DEVICE_H
+#define KNX_DEVICE_H
+
+#include "knx_device/system7/assoc.h"
+#include "knx_device/system7/com_object.h"
+#include "knx_device/system7/device.h"
+#include "knx_device/system7/events.h"
+#include "knx_device/system7/memory.h"
+#include "knx_device/system7/property.h"
+#include "knx_device/system7/tables.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* KNX_DEVICE_H */
+/** @} */
